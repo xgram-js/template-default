2b703e63cb3f7e82a38b9aaa6183d62880b8f38d
RashingPro <rashingpro@yandex.ru>
2025-08-24 21:59:44 +0300

    refactor: small change

--- .xgram-template/prettier/index.js
+++ .xgram-template/prettier/index.js
@@ -3,11 +3,11 @@ import path from "node:path";
 
 export default async function prettier({cwd, installPackage}) {
     const templateDir = path.join(cwd, ".xgram-template");
-    [
+    await installPackage([
         {name: "prettier", type: "dev"},
         {name: "@trivago/prettier-plugin-sort-imports", type: "dev"},
         {name: "prettier-plugin-unused-imports-configurable", type: "dev"}
-    ].map(async (p) => await installPackage(p, cwd))
+    ], cwd)
     fs.appendFileSync(path.join(cwd, ".prettierrc"), fs.readFileSync(path.join(templateDir, "prettier", ".prettierrc"), "utf-8"))
 
     const packageJsonPath = path.join(cwd, "package.json")
