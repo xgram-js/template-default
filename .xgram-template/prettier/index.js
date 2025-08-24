726ab13010b185bcdeb308607d84089458976857
RashingPro <rashingpro@yandex.ru>
2025-08-24 21:53:02 +0300

    refactor: make prettier hook async

--- .xgram-template/prettier/index.js
+++ .xgram-template/prettier/index.js
@@ -2,13 +2,13 @@ import {exec} from "node:child_process";
 import * as fs from "node:fs";
 import path from "node:path";
 
-export default function prettier({cwd, installPackage}) {
+export default async function prettier({cwd, installPackage}) {
     const templateDir = path.join(cwd, ".xgram-template");
     [
         {name: "prettier", type: "dev"},
         {name: "@trivago/prettier-plugin-sort-imports", type: "dev"},
         {name: "prettier-plugin-unused-imports-configurable", type: "dev"}
-    ].map(p => installPackage(p, cwd))
+    ].map(async p => await installPackage(p, cwd))
     fs.appendFileSync(path.join(cwd, ".prettierrc"), fs.readFileSync(path.join(templateDir, "prettier", ".prettierrc"), "utf-8"))
 
     const packageJsonPath = path.join(cwd, "package.json")
